7b6c0a98a73cc0b0f40e6398d001cbc00d8d222e
pardeep kaur <137564227+Pardeep32@users.noreply.github.com>
2024-02-23 16:08:56 -0500

    Create meta for each ec2 map.tf

---
+++ for-each/meta for each ec2 map.tf
@@ -0,0 +1,48 @@
+
+variable "instance_types" {
+  type = map
+  default = {
+    "dev"  = "t2.micro"
+    "test" = "t2.medium"
+    "prod" = "t2.small"
+  }
+}
+
+variable "ami" {
+  type = map
+  default = {
+    "dev"  = "ami-0156b61643fdfee5c"  # Linux
+    "test" = "ami-0a2e7efb4257c0907"  # Ubuntu
+    "prod" = "ami-0a2e7efb4257c0907"  # Ubuntu
+  }
+}
+
+variable "security_groups" {
+  type = map
+  default = {
+    "t2.micro"  = "sg-01276740319f5e0fc"
+    "t2.medium" = "sg-022f51287481b76dd"
+    "t2.small"  = "sg-0ffa6d3eb6eb37f38"
+  }
+}
+
+###### instance creating using for each map ######
+
+resource "aws_instance" "my_instances" {
+  for_each = var.instance_types
+
+  ami           = var.ami[each.key]
+  instance_type = each.value
+  // security_groups = [lookup(var.security_groups, each.value)]  # Use lookup to get the security group ID
+
+  tags = {
+    Name = each.key
+  }
+}
+
+
+# IAM User creation using for_each (set of strings)
+resource "aws_iam_user" "iamuser" {
+  for_each = toset(["deeppp", "kamal", "nanu"])
+  name     = each.key
+}
